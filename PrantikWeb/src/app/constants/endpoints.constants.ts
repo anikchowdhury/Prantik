6a6b0b3da3b6d8174d6b52cb5fe1313f6cac3a99
Anik Chowdhury <sonanik10@gmail.com>
2018-09-10 02:37:43 +0530

    Minor Changes For Deployment

--- PrantikWeb/src/app/constants/endpoints.constants.ts
+++ PrantikWeb/src/app/constants/endpoints.constants.ts
@@ -2,3 +2,8 @@ export const PostUserEndpointContsant = 'http://localhost:58335/api/Users';
 export const PostRoomBookingEndpointContsant = 'http://localhost:58335/api/RoomBookings';
 export const PostBookingDetailsEndpointContsant = 'http://localhost:58335/api/BookingDetails';
 export const PostBookingDetailsUsersEndpointContsant = 'http://localhost:58335/api/BookingDetailsUsers';
+
+/*export const PostUserEndpointContsant = '/api/Users'; 
+export const PostRoomBookingEndpointContsant = '/api/RoomBookings';
+export const PostBookingDetailsEndpointContsant = '/api/BookingDetails';
+export const PostBookingDetailsUsersEndpointContsant = '/api/BookingDetailsUsers';*/
