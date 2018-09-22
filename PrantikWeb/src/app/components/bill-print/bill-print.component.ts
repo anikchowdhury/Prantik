98616134c2af325375f9b57bc65c55c07938f3e3
Anik Chowdhury <sonanik10@gmail.com>
2018-09-23 03:28:45 +0530

    Changes For Biller Select

--- PrantikWeb/src/app/components/bill-print/bill-print.component.ts
+++ PrantikWeb/src/app/components/bill-print/bill-print.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { RoomBookingModel } from '../../models/room-booking.model';
 import { UserModel } from '../../models/user.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import { BookingDetailsService } from '../../services/booking-details.service';
 import { BookingDetailsModel } from '../../models/booking-details.model';
 import { switchMap } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     templateUrl: './bill-print.component.html',
@@ -12,13 +13,17 @@ import { switchMap } from 'rxjs/operators';
     providers: [BookingDetailsService]
 })
 export class BillPrintComponent implements OnInit {
+
+    @ViewChild('billerSelect') selectUserModal: TemplateRef<any>
+
     roomBookings: RoomBookingModel[];
+    users: UserModel[];
     customerDetail: UserModel;
     totalAmount: number;
     qrValue: string;
     gstTotal: number;
 
-    constructor(private route: ActivatedRoute, private bookingDetailsService: BookingDetailsService) {
+    constructor(private route: ActivatedRoute, private bookingDetailsService: BookingDetailsService, private modalService: NgbModal) {
         this.roomBookings = [];
         this.totalAmount = 0;
         this.qrValue = '';
@@ -29,13 +34,15 @@ export class BillPrintComponent implements OnInit {
             phoneNumber: '',
             comingFrom: ''
         };
+        this.users = [];
     }
     ngOnInit() {
         this.route.paramMap.pipe(
             switchMap((params: Params) =>
                 this.bookingDetailsService.GetBookingDetails(params.get('bookingCode'))
             )).subscribe((response: BookingDetailsModel) => {
-                this.customerDetail = response.users[0];
+                this.users = response.users;
+                this.openModal(this.selectUserModal);
                 this.roomBookings = response.rooms;
                 this.roomBookings.forEach((value) => {
                     this.totalAmount += value.amount;
@@ -47,4 +54,13 @@ export class BillPrintComponent implements OnInit {
                     console.log(err);
                 });
     }
+
+    setUserForBill(userModel: UserModel) {
+        this.customerDetail = userModel;
+        this.modalService.dismissAll();
+    }
+
+    private openModal(content) {
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg', backdrop: 'static', keyboard: false });
+      }
 }
